1119fef833d31ba918744948c4c5fe36ebbc7f22
Ashwin Muthiah <muthiah.ashwin2015@gmail.com>
2013-10-30 17:03:10 -0400

    Same..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,36 +7,36 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
-  def create
-  	@user = User.new(params[:user])
-  	if @user.save
-  		redirect_to users_path
-    else
-      redirect_to new_user_path
-  	end
-	end
+ #  def create
+ #  	@user = User.new(params[:user])
+ #  	if @user.save
+ #  		redirect_to root_path
+ #    else
+ #      redirect_to new_user_session_path
+ #  	end
+	# end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  # end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      redirect_to user_path(@user.id)
-    else
-      render 'edit'
-    end
-  end
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update_attributes(params[:user])
+  #     redirect_to user_path(@user.id)
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to users_path
-  end
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   redirect_to users_path
+  # end
 
 end
